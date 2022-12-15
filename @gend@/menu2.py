990d6d6b0f35acc31c057df179d099d669ff2188
jaelps <jaelsonpsilva18@gmail.com>
2022-12-15 00:40:03 -0300

    versao - teste incluido a função 5,4

--- @gend@/menu2.py
+++ @gend@/menu2.py
@@ -1,7 +1,10 @@
 import pandas
 import numpy
 
-print('''
+
+optionSecundario = ''
+while optionSecundario != 7:
+        print('''
          ========================== [ @gend@ ] ========================
         ||                                                           ||
         ||                  [ 1 ] Adicionar Contato                  ||
@@ -9,90 +12,88 @@ print('''
         ||                  [ 3 ] Pesquisar Contato                  ||
         ||                  [ 4 ] Remover Contato                    ||
         ||                  [ 5 ] Alterar Contato                    ||
-        ||                  [ 6 ] Exluir Contato do Usuário          ||
+        ||                  [ 6 ] Exluir Conta do Usuário            ||
         ||                  [ 7 ] Sair                               ||
         ||                                                           ||
         ===============================================================
         ''')
-optionSecundario = int(input('Digite a opção desejada: '))
-
-while optionSecundario != 7:
+        optionSecundario = int(input('Digite a opção desejada: '))
 
         if optionSecundario == 1:
-                while noC != 'n':
+                print('\n--------[ Novo contato ]-----------')
+                nomeContato = str(input('Nome Contato: ')).strip().lower()
 
-                        print('\n--------[ Novo contato ]-----------')
+                numeroContato = str(input('Numero Telefone: ')).strip().lower()
+                digitosN = len(numeroContato)
+
+                if numeroContato not in '1234567890' and digitosN != 11:
+                        print('\n[ Erro é necessario 11 digitos numericos ]\n')
                                                                                                         
-                        nomeContato = str(input('Nome Contato: ')).strip().lower()
-
-                        numeroContato = str(input('Numero Telefone: ')).strip().lower()
-                        digitosN = len(numeroContato)
-
-                        if numeroContato not in '1234567890' and digitosN != 11:
-                                        print('\n[ Erro é necessario 11 digitos numericos ]\n')
-                                                                                                        
-                        verificadoArquivo = '/projeto-ape/' + log_entrada + '.csv'
+                verificadoArquivo = '/projeto-ape/' + log_entrada + '.csv'
                                                                                                 
-                        novoContato = (f'\n{nomeContato},{numeroContato},')
+                novoContato = (f'\n{nomeContato},{numeroContato},')
 
-                        novaAgenda = log_entrada + '.csv'
+                novaAgenda = log_entrada + '.csv'
 
-                        with open(novaAgenda, "a", encoding='utf-8') as dado:
-                                dado.write(novoContato)
+                with open(novaAgenda, "a", encoding='utf-8') as dado:
+                        dado.write(novoContato)
 
 
 
-                        print('[ Novo contato foi adicionado ]')
-                        print('[ Deseja adnicionar novo contato ]')
-                        print('Sim s/n Não')
+                print('[ Novo contato foi adicionado ]')
+                print('[ Deseja adnicionar novo contato ]')
+                print('Sim s/n Não')
 
-                        noC = str(input('Opção: '))
+                noC = str(input('Opção: '))
                                                                 
         elif optionSecundario == 2:
                 tabela = pandas.read_csv('text.csv')
                 print(tabela)
-                continue
+        
 
         elif optionSecundario == 3:
                 tabela = pandas.read_csv('text.csv')  # abertura de arquivo para leitura conforme o guia 
+                
                                 
-                contato =  str(tabela['Nome'])
+                #contato =  str(tabela['Nome'])
+                
 
                 pesquisa = str(input('Pesquisar contato: '))
-                verifi = contato.count(pesquisa)
+                #verifi = contato.count(pesquisa)
+                
 
-                print(contato)
-                print(pesquisa)
-                print(verifi)
+                #print(contato)
+                #print(pesquisa)
+                #print(verifi)
                 
         elif optionSecundario == 4:
-                #print('[ Informar nome para exclusão ]')
-
-                #contatoExcluir = str(input('Nome do contato: '))
-                # Cria um DataFrame a partir de um arquivo csv
-                data = pandas.read_csv("text.csv")
-                #data = data.drop('maria')
                 
-                #selecao = str(data['Nome'] == 'maria')
-                #position = [selecao]
-                itens = data.tz_localize[0,'Nome']
-                #resosta = numpy.array_split(position,10)
-                print(itens)
+                data = pandas.read_csv("text.csv")
+                print(data)
+                delete = int(input('Pesquisar contato: '))
+                result = data.drop(delete)
+                result = result
+                print(result)
 
+                #novaAgenda = log_entrada + '.csv'
+                with open('text.csv', "w", encoding='utf-8') as dado:
+                        dado.write(result)
 
-                break
-                #data.drop(position)
-                '''df = pandas.DataFrame(data, columns = ['Nome', 'Numero'])
-                indexNames = df[df['Name'] == 'maria'].index
-                df.drop(indexNames, inplace=True)'''
+        elif optionSecundario == 5:
+                data = pandas.read_csv("text.csv")
+                print(data)
+                alterar = int(input('Digite a linha do constato a ser alterado:  '))
+                nome = str(input('Nome:  '))
+                numero = int(input('Numero:  '))
+                data['Nome'][alterar] = nome
+                data['Numero'][alterar] = numero
+                print(data)
+                altrado = dado
 
-                data.drop(0)
+                with open('text.csv', "w", encoding='utf-8') as dado:
+                        dado.write(altrado)
 
-                # Descarta os valores passados
-                #tips = tips.drop(line=)
-
-                #elif optionSecundario == 5:
-                #elif optionSecundario == 6:
+        #elif optionSecundario == 6:
         elif optionSecundario == 7:
                 break
         else:
